07ce0d3df69c6b09a0634443e8b7919a4b233d5a
sebastian bastidas <sbastidasr@gmail.com>
2020-02-13 14:44:18 -0500

    Initial app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import './App.css';
+import styled from "styled-components";
+import MyComponent from "./MyComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <AppContainer>
+          <AppHeader>
+            <AppLogo src={logo} alt="logo" />
+            <AppTitle>Welcome to React</AppTitle>
+          </AppHeader>
+          <AppIntro>
+            To get started, edit <code>src/App.js</code> and save.
+          </AppIntro>
+          <MyComponent />
+        </AppContainer>
+      </React.Fragment>
+    );
+  }
 }
+const AppContainer = styled.div`
+  text-align: center;
+`;
 
+const AppHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const AppTitle = styled.h1`
+  font-size: 1.5em;
+`;
+
+const AppLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+  color: red;
+`;
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+// import './App.css';
+import styled from "styled-components";
+
+export const MyComponent = () => {
+  return (
+    <>
+      <div>Hiiiiii </div>
+    </>
+  );
+};
+
+export default MyComponent;
