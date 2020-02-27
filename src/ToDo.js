621af08cbcc912c715812983225d6be297be825f
sebastian bastidas <sbastidasr@gmail.com>
2020-02-27 16:44:06 -0500

    Create todos with time

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,29 +1,70 @@
+import moment from "moment";
 import React, { useState } from "react";
 import styled from "styled-components";
 
 const ToDo = () => {
-  // Declare a new state variable, which we'll call "count"
+  let textInput = React.createRef();
+  let timeInput = React.createRef();
   const [todos, setTodos] = useState([
     {
       text: "buy post it notes",
-      completed: false
+      completed: false,
+      due: moment("2020-01-22T05:00:00Z")
     },
     {
       text: "finish css ",
-      completed: true
+      completed: true,
+      due: moment("2020-01-05T22:00:00Z")
     },
     {
       text: "make it pixel-perfect",
-      completed: false
+      completed: false,
+      due: moment("2020-01-17T08:00:00Z")
     }
   ]);
 
+  const toggleItem = index => {
+    const todosCopy = [...todos];
+    todosCopy[index].completed = !todosCopy[index].completed;
+    console.log("toggle", index, todosCopy);
+    setTodos(todosCopy);
+  };
+
+  const addItem = e => {
+    e.preventDefault();
+    const value = textInput.current.value;
+
+    const todosCopy = [
+      ...todos,
+      {
+        text: value,
+        completed: false,
+        due: moment(timeInput.current.value, "hh:mm")
+      }
+    ];
+    textInput.current.value = "";
+    setTodos(todosCopy);
+  };
+
   return (
     <ToDos>
-      {todos.map(todo => (
-        <ToDoItem>
-          {todo.completed ? "☑" : "☐"}
-          <span> {todo.text}</span>
+      <InputLine onSubmit={addItem}>
+        <input
+          type="text"
+          className="input"
+          placeholder="New Todo"
+          ref={textInput}
+        />
+        <input type="time" ref={timeInput} value="00:00" />
+        <input type="submit" value="+" />
+      </InputLine>
+      {todos.map((todo, index) => (
+        <ToDoItem onClick={() => toggleItem(index)}>
+          <span>
+            {todo.completed ? "☑" : "☐"}
+            {todo.text}
+          </span>
+          <span> {todo.due.format("HH:MM A")}</span>
         </ToDoItem>
       ))}
     </ToDos>
@@ -40,6 +81,18 @@ const ToDoItem = styled.div`
   text-align: left;
   border: 1px solid gray;
   padding: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const InputLine = styled.form`
+  text-align: left;
+  border: 1px solid gray;
+  padding: 20px;
+
+  & .input {
+    width: calc(100% - 110px);
+  }
 `;
 
 export default ToDo;
