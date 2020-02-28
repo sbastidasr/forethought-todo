cae6722f9e6e85040eb5dcf0511ac4ca5bbdfe24
sebastian bastidas <sbastidasr@gmail.com>
2020-02-28 09:47:59 -0500

    Background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import ToDo from "./ToDo";
+// import background from "./background.svg";
 
 class App extends React.Component {
   render() {
@@ -13,10 +14,11 @@ class App extends React.Component {
 }
 const AppContainer = styled.div`
   text-align: center;
-  background-color: rgb(90, 99, 223);
+  // background-color: rgb(90, 99, 223);
   width: 100%;
   height: 100%;
   position: fixed;
+  background-image: url("background.svg");
 `;
 
 export default App;
